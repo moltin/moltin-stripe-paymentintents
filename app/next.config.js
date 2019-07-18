ce8f3ec1aabe961d92e62a259da1686cb0bf9007
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-18 11:57:26 +0100

    feat: Add Tailwind CSS (#5)

--- app/next.config.js
+++ app/next.config.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
+const withCSS = require('@zeit/next-css')
 
-module.exports = {
+module.exports = withCSS({
   target: 'serverless',
   env: {
     MOLTIN_CLIENT_ID: process.env.MOLTIN_CLIENT_ID,
     STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY
   }
-}
+})
