b952d6fde5b25d0b2d992d8a721af96c89d64906
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-18 15:52:12 +0100

    feat: Pseudo cart UI

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,12 +1,22 @@
 import { Elements } from 'react-stripe-elements'
 
+import Cart from '../components/Cart'
 import CheckoutForm from '../components/CheckoutForm'
 import '../styles/index.css'
 
 function Index() {
   return (
     <Elements>
-      <CheckoutForm />
+      <main className="mx-auto py-12 w-1/2">
+        <div className="flex -mx-2">
+          <div className="px-2 w-2/3">
+            <CheckoutForm />
+          </div>
+          <div className="px-2 w-1/3">
+            <Cart />
+          </div>
+        </div>
+      </main>
     </Elements>
   )
 }
