0cf472f81bc574bda85f52fd5fc9aec4b30ba66a
Jonathan Steele <ynnoj@users.noreply.github.com>
2019-07-19 13:26:55 +0100

    feat: Better success state on checkout

--- app/components/CheckoutForm.js
+++ app/components/CheckoutForm.js
@@ -71,113 +71,112 @@ function CheckoutForm({ stripe }) {
 
           return (
             <React.Fragment>
-              {submitSucceeded ? (
-                <div className="my-4 text-gray-600 text-center">
+              {submitSucceeded && (
+                <div className="bg-green-200 border border-green-300 my-4 p-2 rounded text-center text-green-600 text-sm">
                   Thank you for your order!
                 </div>
-              ) : (
-                <form onSubmit={handleSubmit}>
-                  <div className="flex -mx-2">
-                    <Field name="name">
-                      {({ input, meta }) => (
-                        <div className="flex flex-col mb-4 px-2 w-1/2">
-                          <label className="block font-medium mb-2 text-sm">
-                            Name
-                          </label>
-                          <input
-                            {...input}
-                            type="text"
-                            disabled={checkingOutCart || addingToCart}
-                            className={`border-2 mb-2 p-2 rounded ${
-                              meta.error && meta.touched
-                                ? 'border-red-400'
-                                : 'border-gray-300'
-                            } ${
-                              checkingOutCart || addingToCart
-                                ? 'opacity-50'
-                                : 'opacity-100'
-                            }`}
-                          />
-                          {meta.error && meta.touched && (
-                            <span className="text-red-400 text-sm">
-                              {meta.error}
-                            </span>
-                          )}
-                        </div>
-                      )}
-                    </Field>
-                    <Field name="email">
-                      {({ input, meta }) => (
-                        <div className="flex flex-col mb-4 px-2 w-1/2">
-                          <label className="block font-medium mb-2 text-sm">
-                            Email address
-                          </label>
-                          <input
-                            {...input}
-                            type="email"
-                            disabled={checkingOutCart || addingToCart}
-                            className={`border-2 mb-2 p-2 rounded ${
-                              meta.error && meta.touched
-                                ? 'border-red-400'
-                                : 'border-gray-300'
-                            } ${
-                              checkingOutCart || addingToCart
-                                ? 'opacity-50'
-                                : 'opacity-100'
-                            }`}
-                          />
-                          {meta.error && meta.touched && (
-                            <span className="text-red-400 text-sm">
-                              {meta.error}
-                            </span>
-                          )}
-                        </div>
-                      )}
-                    </Field>
-                  </div>
-                  <Field name="stripe">
-                    {({ meta }) => (
-                      <div className="mb-4">
+              )}
+              <form onSubmit={handleSubmit}>
+                <div className="flex -mx-2">
+                  <Field name="name">
+                    {({ input, meta }) => (
+                      <div className="flex flex-col mb-4 px-2 w-1/2">
                         <label className="block font-medium mb-2 text-sm">
-                          Card details
+                          Name
                         </label>
-                        <CardElement
-                          onChange={onStripeChange}
-                          disabled={disableButton}
-                          hidePostalCode={true}
-                          className={`border-2 mb-2 px-2 py-4 rounded ${
+                        <input
+                          {...input}
+                          type="text"
+                          disabled={checkingOutCart || addingToCart}
+                          className={`border-2 mb-2 p-2 rounded ${
                             meta.error && meta.touched
                               ? 'border-red-400'
                               : 'border-gray-300'
+                          } ${
+                            checkingOutCart || addingToCart
+                              ? 'opacity-50'
+                              : 'opacity-100'
                           }`}
                         />
                         {meta.error && meta.touched && (
                           <span className="text-red-400 text-sm">
-                            {meta.error.complete}
+                            {meta.error}
                           </span>
                         )}
                       </div>
                     )}
                   </Field>
-                  <button
-                    className={`bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full ${
-                      disableButton
-                        ? 'cursor-not-allowed opacity-50'
-                        : 'opacity-100'
-                    }`}
-                    type="submit"
-                    disabled={disableButton}
-                  >
-                    {checkingOutCart
-                      ? 'Processing'
-                      : addingToCart
-                      ? 'Updating cart'
-                      : cartAmount === 0
-                      ? 'Cart is empty'
-                      : `Checkout and pay ${cartTotal}`}
-                  </button>
-                </form>
-              )}
+                  <Field name="email">
+                    {({ input, meta }) => (
+                      <div className="flex flex-col mb-4 px-2 w-1/2">
+                        <label className="block font-medium mb-2 text-sm">
+                          Email address
+                        </label>
+                        <input
+                          {...input}
+                          type="email"
+                          disabled={checkingOutCart || addingToCart}
+                          className={`border-2 mb-2 p-2 rounded ${
+                            meta.error && meta.touched
+                              ? 'border-red-400'
+                              : 'border-gray-300'
+                          } ${
+                            checkingOutCart || addingToCart
+                              ? 'opacity-50'
+                              : 'opacity-100'
+                          }`}
+                        />
+                        {meta.error && meta.touched && (
+                          <span className="text-red-400 text-sm">
+                            {meta.error}
+                          </span>
+                        )}
+                      </div>
+                    )}
+                  </Field>
+                </div>
+                <Field name="stripe">
+                  {({ meta }) => (
+                    <div className="mb-4">
+                      <label className="block font-medium mb-2 text-sm">
+                        Card details
+                      </label>
+                      <CardElement
+                        onChange={onStripeChange}
+                        disabled={disableButton}
+                        hidePostalCode={true}
+                        className={`border-2 mb-2 px-2 py-4 rounded ${
+                          meta.error && meta.touched
+                            ? 'border-red-400'
+                            : 'border-gray-300'
+                        }`}
+                      />
+                      {meta.error && meta.touched && (
+                        <span className="text-red-400 text-sm">
+                          {meta.error.complete}
+                        </span>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <button
+                  className={`bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded w-full ${
+                    disableButton
+                      ? 'cursor-not-allowed opacity-50'
+                      : 'opacity-100'
+                  }`}
+                  type="submit"
+                  disabled={disableButton}
+                >
+                  {checkingOutCart
+                    ? 'Processing'
+                    : addingToCart
+                    ? 'Updating cart'
+                    : cartAmount === 0
+                    ? 'Cart is empty'
+                    : `Checkout and pay ${cartTotal}`}
+                </button>
+              </form>
             </React.Fragment>
           )
         }}
